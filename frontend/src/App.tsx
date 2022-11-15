a1b7cfe9f5c2faeda901ecba4c048d9811c784d7
Jéssica Pimentel <jppimentel96@hotmail.com>
2022-11-15 15:52:58 -0300

    Outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-    <h1>Olá Mundo</h1>
-    <NotificationButton/>
-    <NotificationButton/>
+    <Header />
+    <main>
+        <section id="sales">
+            <div className="container-card">
+              <SalesCard/>
+            </div>
+        </section>
+    </main>
     </>
   )
 }
